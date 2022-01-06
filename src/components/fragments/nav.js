9a5ea5a18b915900c58d5a0ca10d7fbf9e836561
miszczuq <michal99miszczuk@gmail.com>
2022-01-06 02:10:51 +0100

    repaired nav

--- src/components/fragments/nav.js
+++ src/components/fragments/nav.js
@@ -9,10 +9,10 @@ function Navigation(){
         <nav>
             <div className="nav-content">
                 <ul className="nav-list">
-                    <li><NavLink to="/" exact={true} activeClassName='active'> Strona główna</NavLink></li>
-                    <li><NavLink to="/drivers" exact={true} activeClassName='active'>Kierowcy</NavLink></li>
-                    <li><NavLink to="/gokarts" exact={true} activeClassName='active'>Gokarty</NavLink></li>
-                    <li><NavLink to="/driverGokarts" exact={true} activeClassName='active'>Przejazdy</NavLink></li>
+                    <li><NavLink to="/" activeClassName='active'> Strona główna</NavLink></li>
+                    <li><NavLink to="/drivers" activeClassName='active'>Kierowcy</NavLink></li>
+                    <li><NavLink to="/gokarts" activeClassName='active'>Gokarty</NavLink></li>
+                    <li><NavLink to="/driverGokarts" activeClassName='active'>Przejazdy</NavLink></li>
                 </ul>
             </div>
         </nav>
