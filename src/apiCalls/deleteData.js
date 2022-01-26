21e4faa1355285ca20df6a0c93ac4e7795a420be
miszczuq <michal99miszczuk@gmail.com>
2022-01-26 19:36:31 +0100

    repaired bug

--- src/apiCalls/deleteData.js
+++ src/apiCalls/deleteData.js
@@ -3,17 +3,19 @@ import {getCurrentUser} from "../helpers/authHelper";
 
 export const deleteData = async (route, recordId) => {
     let user = getCurrentUser();
-    let token = user.token;
-    console.log("TokenInDelete", token)
+    let headers = {};
+    if(user) {
+        let token = user.token;
 
-    const headers =  {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer '+token
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
     }
 
-    return axios.delete(`http://localhost:3000/api${route}/${recordId}`,{headers});
-        // .then(res => {
-        //     return res;
-        // })
-        // .catch(err => console.log(err))
+    return axios.delete(`http://localhost:3000/api${route}/${recordId}`,{headers})
+        .then(res => {
+            return res;
+        })
+        .catch(err => err.response);
 }
